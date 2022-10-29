08d9785264fa680c1f6363591421fd1b20341694
Suraj_SJ <jaiswaljesus384@gmail.com>
2022-10-29 09:11:09 +0530

    variable_ch_2

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,29 @@
 void main(){
+  // print function is a inbuild function to print message on the console
   print("hello world");
+
+  // for single line comments we use //
+  /*
+  this is for multi line comments 
+  */
+
+  //#variable Name and Rules
+    //##valid Variable Name
+
+    var name = "suraj";
+    var f_name = "rahul";
+    var address = "pune";
+    var $mony =100;
+
+    /* **** we use the var to decleared 
+    the variable in dart programing **** */
+
+    // ## invalid variable name
+    // var 1person = "sumit";
+    // var for = 1.02;
+
+
+
+
+
 }

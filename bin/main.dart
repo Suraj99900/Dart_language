c3bfa7ff5a3ce7e1b13add62a52d18039bb3bf76
Suraj_SJ <jaiswaljesus384@gmail.com>
2022-10-29 09:27:01 +0530

    to_check_datatypes

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,8 @@
+import 'dart:ffi';
+
 void main(){
   // print function is a inbuild function to print message on the console
-  print("hello world");
+  // print("hello world");
 
   // for single line comments we use //
   /*
@@ -22,8 +24,33 @@ void main(){
     // var 1person = "sumit";
     // var for = 1.02;
 
+    // # printing variable using print function
+
+    // print(name);
+    // print(f_name);
+    // print(address);
+    // print($mony);
 
 
+
+  // built in datatypes in DART programming 
+ /* int cart_item = 10;
+  double cart_item_2 = 10.22;
+  String n_name = "suraj";
+  bool is_true = false;
+  var anything = 1.2;
+  print(cart_item);
+  print(cart_item_2);
+  print(n_name);
+  print(is_true); */
+
+  //to check runtime datatype 
+
+    // print(cart_item.runtimeType);
+    // print(cart_item_2.runtimeType);
+    // print(n_name.runtimeType);
+    // print(is_true.runtimeType);
+    // print(anything.runtimeType);
 
 
 }
